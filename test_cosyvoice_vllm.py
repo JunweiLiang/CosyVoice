25f23166d4b59144612bcf5912a222e7f2b76e82
Junwei Liang <junweiliang1114@gmail.com>
2026-03-18 14:11:46 +0800

    updated

--- test_cosyvoice_vllm.py
+++ test_cosyvoice_vllm.py
@@ -24,6 +24,28 @@ if __name__ == "__main__":
     output_device_idx = sd.default.device[0]
     print("Use this speaker: %s" % devices[output_device_idx]["name"])
 
+
+    # zero_shot usage
+    print("----Loading wav -----")
+    wav_file = './test_audio/laopo2.wav'
+    prompt_speech_16k = load_wav(wav_file, 16000)
+
+    # cosyvoice 3 比 2多了这个prompt: You are a helpful assistant.<|endofprompt|> 必须要加
+    prompt_speech_text = "You are a helpful assistant.<|endofprompt|>现在我们有很多突出的矛盾，比如说人岗不匹配，比如说这个整个学科设置不合理，那么就整个会导致我们培养出来的学生的能力，和真正的市场需求，他是脱节的。那么这个问题为什么会产生呢，一方面是因为现在整个科技的发展在加速，导致整个用工市场，对能力的需求的结构，也是在快速地变化。"
+    print("----Loaded wav -----")
+
+    """
+    cosyvoice = CosyVoice2('./pretrained_models/CosyVoice2-0.5B_vllm/CosyVoice2-0.5B',
+        load_jit=False, load_trt=False, fp16=True, load_vllm=True,
+        prompt_text=prompt_speech_text, prompt_speech_16k=prompt_speech_16k, gpu_memory_utilization=0.5, quantization="fp8")#quantization=None)
+    """
+    print("----Loading model -----")
+    cosyvoice = AutoModel(
+        model_dir='pretrained_models/Fun-CosyVoice3-0.5B', load_vllm=True,
+        prompt_text=prompt_speech_text, prompt_speech_16k=prompt_speech_16k)
+    print("model sample rate from config:" % cosyvoice.sample_rate)
+    print("----load model done -----")
+
     def generate_voice(output):
         for i, j in enumerate(output):
             print(i)
@@ -40,26 +62,6 @@ if __name__ == "__main__":
             sd.wait() # wait till the playback is complete
             # sd.stop() # will make it stop mid play
 
-
-
-    # zero_shot usage
-    print("----Loading wav -----")
-    wav_file = './test_audio/laopo2.wav'
-    prompt_speech_16k = load_wav(wav_file, 16000)
-
-    prompt_speech_text = "You are a helpful assistant.<|endofprompt|>现在我们有很多突出的矛盾，比如说人岗不匹配，比如说这个整个学科设置不合理，那么就整个会导致我们培养出来的学生的能力，和真正的市场需求，他是脱节的。那么这个问题为什么会产生呢，一方面是因为现在整个科技的发展在加速，导致整个用工市场，对能力的需求的结构，也是在快速地变化。"
-    print("----Loaded wav -----")
-
-    """
-    cosyvoice = CosyVoice2('./pretrained_models/CosyVoice2-0.5B_vllm/CosyVoice2-0.5B',
-        load_jit=False, load_trt=False, fp16=True, load_vllm=True,
-        prompt_text=prompt_speech_text, prompt_speech_16k=prompt_speech_16k, gpu_memory_utilization=0.5, quantization="fp8")#quantization=None)
-    """
-    print("----Loading model -----")
-    cosyvoice = AutoModel(
-        model_dir='pretrained_models/Fun-CosyVoice3-0.5B', load_vllm=True,
-        prompt_text=prompt_speech_text, prompt_speech_16k=prompt_speech_16k)
-    print("----load model done -----")
 
     # 微信语音，然后用苹果电脑quicktime录声音，然后转
     # ffmpeg -i /Users/junweiliang/Downloads/zero_shot_prompt_laopo.m4a -ss 00:00:03 -to 00:00:06 -acodec pcm_s16le -ac 1 -ar 16000 zero_shot_prompt_laopo.wav
